24e694a9af09eaf11250a8a5346a76baa19216d2
0200832211005 <0200832211005@LAB03-03>
2025-03-12 22:03:39 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+
+        public double QntAdultos { get; set; }
+        public double QntCriancas { get; set;}
+
+        public DateTime DataCheckIn { get; set; }
+
+        public DateTime DataCheckOut { get; set; }
+        public int Estadia
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QntAdultos * QuartoSelecionado.ValorDiariaAlduto;
+                double valor_criancas = QntCriancas * QuartoSelecionado.ValorDiariaCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadia;
+
+                return total;
+
+            }
+        }
+    }
+}
---
+++ MauiAppHotel/Views/HospedagemContratacacao1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratacacao1 : ContentPage
+{
+	public HospedagemContratacacao1()
+	{
+		InitializeComponent();
+	}
+}
